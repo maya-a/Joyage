6f0814db7b64464bcc3adf98540c2eb41e7bccb0
maya aderka <mayaad0@gmail.com>
2019-06-30 17:29:46 +0300

    added airports

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,30 @@
 class SearchesController < ApplicationController
   def new
+    @search = Search.new()
   end
 
   def create
+    @search = Search.new (search_params)
+    @trip = Trip.new
+
+
+  private
+
+  def search_params
+    params.require(:search).permit(:max_budget, :dep_date, :ret_date, :origin)
   end
+end
+
+
+a = Geokit::LatLng.new(13.0627081,80.274658)
+b = Geokit::LatLng.new(12.8550615,80.2264393)
+
+m1 = a.midpoint_to(b)
+m2 = b.midpoint_to(a)
+
+
+if m1.distance_to( m2 ) > 0
+  puts "Mid-points are separate"
+else
+  puts "Mid-points are the same location"
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,17 +1,32 @@
 class Trip < ApplicationRecord
   belongs_to :search
-
-  # require "csv"
-  # airports = []
-  # CSV.foreach("../../db/airports.csv") do |row|
-  #   airports << {
-  #                  id: row[0],
-  #                  name: row[1],
-  #                  city: row[2],
-  #                  country: row[3],
-  #                  code: row[4],
-  #                  Latitude: row[6],
-  #                  Longitude: row[7]
-  #               }
-  # end
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  require "csv"
+  airports = []
+  CSV.foreach("../../db/airports.csv") do |row|
+    airports << {
+                   id: row[0],
+                   name: row[1],
+                   city: row[2],
+                   country: row[3],
+                   code: row[4],
+                   latitude: row[6],
+                   longitude: row[7]
+                }
+  # CSV row
+  # [0] airport ID
+  # [1] airport name
+  # [2] city name
+  # [3] country name
+  # [4] code (IATA/FAA)
+  # [5] ICAO
+  # [6] latitude
+  # [7] longitude
+  # [8] elevation (height)
+  # [9] UTC
+  # [10] DST (AREA)
+  # [11] "airport"
+  # [12] "OurAirports"
+  end
   end
