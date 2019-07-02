66223a0b73fe57ee2e9d08f4b5a6bdd155f7fbf3
Joseph Kanner <josephkanner@gmail.com>
2019-07-02 14:11:28 +0300

    algolia stuff

---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,11 @@
+AlgoliaSearch.configuration = { application_id: '6WKL3F66N2', api_key: 'e4374f679cd13ee3e22ad7c67ab7c1c0' }
+
+AlgoliaSearch.configuration = {
+  application_id: '6WKL3F66N2',
+  api_key: 'e4374f679cd13ee3e22ad7c67ab7c1c0',
+  connect_timeout: 2,
+  receive_timeout: 30,
+  send_timeout: 30,
+  batch_timeout: 120,
+  search_timeout: 5
+}
