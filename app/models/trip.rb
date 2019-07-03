cbd899e512c116e4301ebdbee7105c478a47dcfe
maya aderka <mayaad0@gmail.com>
2019-07-03 19:12:04 +0300

    got new origin seeds + show info

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   belongs_to :search
   belongs_to :destination
-  has_many :itineraries, dependent: :destroy
+  has_many :itinerarys, dependent: :destroy
   has_many :flights, through: :itinerarys, dependent: :destroy
 end
 
