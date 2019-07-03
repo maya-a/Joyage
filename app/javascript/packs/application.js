8acf88a025b5be26f1e6b1f36d39484eded68307
(Maximilian Pfadenhauer) <max.pfadenhauer@gmail.com>
2019-07-03 11:49:08 +0300

    2 card options

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import "../plugins/slider";
 import "../plugins/add-origins";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
-import 'select2/dist/css/select2.css'
+import 'select2/dist/css/select2.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initSelect2 } from '../plugins/init_select2';
 
