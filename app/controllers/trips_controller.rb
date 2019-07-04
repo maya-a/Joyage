53fc3b826c459c5ab942ddd92a5e4cec798641e5
maya aderka <mayaad0@gmail.com>
2019-07-04 12:17:21 +0300

    added multiple itineraries

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -40,54 +40,61 @@ class TripsController < ApplicationController
         #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
       # }
 
-    @trips = Trip.where.not(latitude: nil, longitude: nil)
-    @markers = []
-    2.times do
-      @markers << {
-        lat: 25.02424,
-        lng: 41.81812,
-        #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
-        #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
-      }
+    # @trips = Trip.where.not(latitude: nil, longitude: nil)
+    # @markers = []
+    # 2.times do
+    #   @markers << {
+    #     lat: 25.02424,
+    #     lng: 41.81812,
+    #     #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
+    #     #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+    #   }
 
-          @markers << {
-        lat: 32.080351,
-        lng: 34.767633,
-        #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
-        #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
-      }
-            @markers << {
-        lat: 10.080351,
-        lng: 12.767633,
-        #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
-        #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
-      }
-    end
+    #       @markers << {
+    #     lat: 32.080351,
+    #     lng: 34.767633,
+    #     #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
+    #     #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+    #   }
+    #         @markers << {
+    #     lat: 10.080351,
+    #     lng: 12.767633,
+    #     #infoWindow: render_to_string(partial: "infowindow", locals: { trip: trip }),
+    #     #image_url: helpers.asset_url('REPLACE_THIS_WITH_YOUR_IMAGE_IN_ASSETS')
+    #   }
+    # end
 
-  end
 
   def show
-    trip = Trip.find(params[:id])
+    @trips = []
+    @trips << Trip.find(params[:id])
+    @trips << Trip.find(params[:id].to_i + 1)
+    @trips << Trip.find(params[:id].to_i + 2)
+       @flight_info = []
       # getting first flight only
-      @flight_info = {
-        destination_code: eval(trip.itineraries[0].info)[0][0][:destination],
-        price:            eval(trip.itineraries[0].info)[0][0][:price],
+      @trips.each do |trip|
+          trip.itineraries.each do |itinerary|
+            @flight_info << {
+              itinerary_index: itinerary.id ,
+              destination_code: eval(itinerary.info)[0][0][:destination],
+              price:            eval(itinerary.info)[0][0][:price],
 
-        origin_city:    eval(trip.itineraries[0].info)[0][1][:origin_city],
-        arrival_city:   eval(trip.itineraries[0].info)[0][1][:arrival_city],
-        departure_date: eval(trip.itineraries[0].info)[0][1][:departure_date],
-        arrival_date:   eval(trip.itineraries[0].info)[0][1][:arrival_date],
-        layovers:       eval(trip.itineraries[0].info)[0][1][:layovers],
-        duration:       eval(trip.itineraries[0].info)[0][1][:duration],
+              origin_city:    eval(itinerary.info)[0][1][:origin_city],
+              arrival_city:   eval(itinerary.info)[0][1][:arrival_city],
+              departure_date: eval(itinerary.info)[0][1][:departure_date],
+              arrival_date:   eval(itinerary.info)[0][1][:arrival_date],
+              layovers:       eval(itinerary.info)[0][1][:layovers],
+              duration:       eval(itinerary.info)[0][1][:duration],
 
-        return_origin_city:    eval(trip.itineraries[0].info)[0][2][:return_origin_city],
-        return_arrival_city:   eval(trip.itineraries[0].info)[0][2][:return_arrival_city],
-        return_departure_date: eval(trip.itineraries[0].info)[0][2][:return_departure_date],
-        return_arrival_date:   eval(trip.itineraries[0].info)[0][2][:return_arrival_date],
-        return_layovers:       eval(trip.itineraries[0].info)[0][2][:return_layovers],
-        return_duration:       eval(trip.itineraries[0].info)[0][2][:return_duration]
-     }
-
+              return_origin_city:    eval(itinerary.info)[0][2][:return_origin_city],
+              return_arrival_city:   eval(itinerary.info)[0][2][:return_arrival_city],
+              return_departure_date: eval(itinerary.info)[0][2][:return_departure_date],
+              return_arrival_date:   eval(itinerary.info)[0][2][:return_arrival_date],
+              return_layovers:       eval(itinerary.info)[0][2][:return_layovers],
+              return_duration:       eval(itinerary.info)[0][2][:return_duration]
+           }
+          end
+        end
     #@tag = "https://source.unsplash.com/1600x900/?#{@cocktail.name.gsub(' ', '-')}"
   end
 
@@ -122,3 +129,4 @@ class TripsController < ApplicationController
 
 # eval(@trips[0].itineraries.first.info)[0][0][:price]
 # eval(@trips[0].itineraries.first.info)[0][0][:destination]
+  end
