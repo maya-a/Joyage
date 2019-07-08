ea858c529cf21a8c93d9d17b3fdd6ac3b875faab
Charlotte Hetzler <charlotte@hetzler.de>
2019-07-08 10:19:23 +0300

    Closed all divs

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,7 +3,6 @@ class TripsController < ApplicationController
     @list = params[:list_destinations].split(',')
 
     @trips = Trip.where(search: params[:search_id])
-
     # @itineraries = @trips[0].itineraries
     @search = Search.find(params[:search_id])
     @destinations = []
@@ -38,35 +37,36 @@ class TripsController < ApplicationController
       end
     end
     @travel = @trips.group_by { |d| d[:destination_id] }
+    raise
     @flights_infos = []
-      # getting first flight only
-      @travel.each do |did, trip|
-        trip = Trip.find(trip[0].id)
-        trip.itineraries.each do |itinerary|
-          itinerary = Itinerary.find(itinerary.id)
-          @flights_infos << {
-            itinerary_index: itinerary.id ,
-            destination_code: eval(itinerary.info)[0][:destination],
-            price:            eval(itinerary.info)[0][:price],
-            destination_city: Destination.find(trip.destination.id).d_city,
-            origin_city_name: Origin.find_by(code: eval(itinerary.info)[1][:origin_city]).city,
+    # getting first flight only
+    @travel.each do |did, trip|
+      trip = Trip.find(trip[0].id)
+      trip.itineraries.each do |itinerary|
+        itinerary = Itinerary.find(itinerary.id)
+        @flights_infos << {
+          itinerary_index: itinerary.id ,
+          destination_code: eval(itinerary.info)[0][:destination],
+          price:            eval(itinerary.info)[0][:price],
+          destination_city: Destination.find(trip.destination.id).d_city,
+          origin_city_name: Origin.find_by(code: eval(itinerary.info)[1][:origin_city]).city,
 
-            origin_city:    eval(itinerary.info)[1][:origin_city],
-            arrival_city:   eval(itinerary.info)[1][:arrival_city],
-            departure_date: eval(itinerary.info)[1][:departure_date],
-            arrival_date:   eval(itinerary.info)[1][:arrival_date],
-            layovers:       eval(itinerary.info)[1][:layovers],
-            duration:       eval(itinerary.info)[1][:duration],
+          origin_city:    eval(itinerary.info)[1][:origin_city],
+          arrival_city:   eval(itinerary.info)[1][:arrival_city],
+          departure_date: eval(itinerary.info)[1][:departure_date],
+          arrival_date:   eval(itinerary.info)[1][:arrival_date],
+          layovers:       eval(itinerary.info)[1][:layovers],
+          duration:       eval(itinerary.info)[1][:duration],
 
-            return_origin_city:    eval(itinerary.info)[2][:return_origin_city],
-            return_arrival_city:   eval(itinerary.info)[2][:return_arrival_city],
-            return_departure_date: eval(itinerary.info)[2][:return_departure_date],
-            return_arrival_date:   eval(itinerary.info)[2][:return_arrival_date],
-            return_layovers:       eval(itinerary.info)[2][:return_layovers],
-            return_duration:       eval(itinerary.info)[2][:return_duration]
-          }
-        end
+          return_origin_city:    eval(itinerary.info)[2][:return_origin_city],
+          return_arrival_city:   eval(itinerary.info)[2][:return_arrival_city],
+          return_departure_date: eval(itinerary.info)[2][:return_departure_date],
+          return_arrival_date:   eval(itinerary.info)[2][:return_arrival_date],
+          return_layovers:       eval(itinerary.info)[2][:return_layovers],
+          return_duration:       eval(itinerary.info)[2][:return_duration]
+        }
       end
+    end
   end
 
 
