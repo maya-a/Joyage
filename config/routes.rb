29fa25cc0f076d2e81dd6ee4dc2901c793a3cca9
maya aderka <mayaad0@gmail.com>
2019-07-09 20:02:57 +0300

    added an about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :destinations, only: :show
   end
   get "thankyou", to: "pages#thankyou"
+  get "about", to: "pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "pages/loading", to: 'pages#loading_page'
 end
